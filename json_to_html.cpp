215e46ec9440b34551ea1122567bcbf468196623
mattiaunibo <guidetti.mattia@gmail.com>
2015-11-23 15:33:40 +0100

    added a time information to the marker

--- json_to_html.cpp
+++ json_to_html.cpp
@@ -131,7 +131,18 @@ int main(int argc, char** argv){
   if( gps_records.type() == 2 )                         //array-style
 	  for (size_t i = 0; i < gps_records.size(); ++i){
 	    try{
-	      output_file << "[" << std::fixed << std::setprecision(6) << gps_records[i]["lat"].as<double>() << "," << gps_records[i]["lon"].as<double>() << "]" << (i!=gps_records.size()-1?',':' ') << "\n";
+	      //output_file << "[" << std::fixed << std::setprecision(6) << gps_records[i]["lat"].as<double>() << "," << gps_records[i]["lon"].as<double>() << "]" << (i!=gps_records.size()-1?',':' ') << "\n";
+	      output_file 
+		  << "[" 
+		  << std::fixed << std::setprecision(6) 
+		  << gps_records[i]["lat"].as<double>() 
+		  << "," 
+		  << gps_records[i]["lon"].as<double>() 
+		  <<",'"
+		  <<gps_records[i]["timestamp"].as<std::string>() 
+		  << "']" 
+		  << (i!=gps_records.size()-1?',':' ') 
+		  << "\n";
 	    }
 	    catch (const std::exception& e){
 	      std::cerr << e.what() << std::endl;
@@ -141,7 +152,17 @@ int main(int argc, char** argv){
 		int i = 0;
 		for(auto rec = gps_records.begin_members(); rec != gps_records.end_members(); ++rec, ++i) {
 			try{
-	    	output_file << "[" << std::fixed << std::setprecision(6) << rec->value()["lat"].as<double>() << "," << rec->value()["lon"].as<double>() << "]" << (i!=gps_records.size()-1?',':' ') << "\n";
+	    	output_file 
+			<< "[" 
+			<< std::fixed << std::setprecision(6) 
+			<< rec->value()["lat"].as<double>() 
+			<< "," 
+			<< rec->value()["lon"].as<double>() 
+			<<",'" 
+			<<rec->value()["timestamp"].as<std::string>()
+			<< "']" 
+			<< (i!=gps_records.size()-1?',':' ') 
+			<< "\n";
 	  	}
 		  catch (const std::exception& e){
 		    std::cerr << e.what() << std::endl;
