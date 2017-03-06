1c7e5285e70aedb81bd444d755244069806b2e03
Alessandro Fabbri <rommacc@yahoo.it>
2017-03-06 18:05:18 +0100

    add auto boundary detection, move error messages to stderr

--- src/json_to_html.cpp
+++ src/json_to_html.cpp
@@ -35,20 +35,20 @@ using namespace std;
 
 void usage(char* progname) {
   // Usage
-  std::cout << "Usage: " << progname << " -i [input.json] -o [output.html] -u [undersampling] -s -[m|p]" << std::endl;
-  std::cout << "\t- [input.json] Physycom GNSS .json format file to parse" << std::endl;
-  std::cout << "\t- [output.html] html script to display route in Google Maps (only)" << std::endl;
-  std::cout << "\t- [undersampling] a positive integer representing the undersampling factor" << std::endl;
-  std::cout << "\t- -s optional flag for short point description (timestamp only)" << std::endl;
-  std::cout << "\t- -[m|p] optional flag for markers (m), polyline (p), none for both" << std::endl;
+  cout << "Usage: " << progname << " -i [input.json] -o [output.html] -u [undersampling] -s -[m|p]" << endl;
+  cout << "\t- [input.json] Physycom GNSS .json format file to parse" << endl;
+  cout << "\t- [output.html] html script to display route in Google Maps (only)" << endl;
+  cout << "\t- [undersampling] a positive integer representing the undersampling factor" << endl;
+  cout << "\t- -s optional flag for short point description (timestamp only)" << endl;
+  cout << "\t- -[m|p] optional flag for markers (m), polyline (p), none for displaying both" << endl;
   exit(1);
 }
 
 int main(int argc, char** argv) {
-  std::cout << "Json_to_Html (for Google Maps only) v" << MAJOR_VERSION << "." << MINOR_VERSION << std::endl;
+  cout << "Json_to_Html (for Google Maps only) v" << MAJOR_VERSION << "." << MINOR_VERSION << endl;
 
   // Parsing command line
-  std::string input_name, output_name, mode;
+  string input_name, output_name, mode;
   size_t undersampling = 1;
   bool verbose = true;
   bool show_markers = true;
@@ -76,18 +76,18 @@ int main(int argc, char** argv) {
           show_markers = false;
           break;
         default:    // no match...
-          std::cout << "ERROR: Flag \"" << argv[i] << "\" not recognized. Quitting..." << std::endl;
+          cerr << "ERROR: Flag \"" << argv[i] << "\" not recognized. Quitting..." << endl;
           usage(argv[0]);
         }
       }
       else {
-        std::cout << "ERROR: Flag \"" << argv[i] << "\" not recognized. Quitting..." << std::endl;
+        cerr << "ERROR: Flag \"" << argv[i] << "\" not recognized. Quitting..." << endl;
         usage(argv[0]);
       }
     }
   }
   else {
-    std::cout << "ERROR: No flags specified. Read usage and relaunch properly." << std::endl;
+    cerr << "ERROR: No flags specified. Read usage and relaunch properly." << endl;
     usage(argv[0]);
   }
 
@@ -98,18 +98,18 @@ int main(int argc, char** argv) {
 
   if (input_name.size() > 5) {
     if (input_name.substr(input_name.size() - 5, 5) != ".json") {
-      std::cout << input_name << " is not a valid .json file. Quitting..." << std::endl;
+      cerr << input_name << " is not a valid .json file. Quitting..." << endl;
       exit(2);
     }
   }
   else {
-    std::cout << input_name << " is not a valid .json file. Quitting..." << std::endl;
+    cerr << input_name << " is not a valid .json file. Quitting..." << endl;
     exit(22);
   }
 
   input_file.open(input_name.c_str());
   if (!input_file.is_open()) {
-    cout << "FAILED: Input file " << input_name << " could not be opened. Quitting..." << std::endl;
+    cerr << "FAILED: Input file " << input_name << " could not be opened. Quitting..." << endl;
     exit(222);
   }
   else { cout << "SUCCESS: file " << input_name << " opened!\n"; }
@@ -117,24 +117,28 @@ int main(int argc, char** argv) {
 
   if (output_name.size() > 5) {
     if (output_name.substr(output_name.size() - 5, 5) != ".html") {
-      std::cout << output_name << " is not a valid .html file. Quitting..." << std::endl;
+      cerr << output_name << " is not a valid .html file. Quitting..." << endl;
       exit(3);
     }
   }
   else {
-    std::cout << output_name << " is not a valid .html file. Quitting..." << std::endl;
+    cerr << output_name << " is not a valid .html file. Quitting..." << endl;
     exit(33);
   }
 
   output_file.open(output_name.c_str());
   if (!output_file.is_open()) {
-    cout << "FAILED: Output file " << output_name << " could not be opened. Quitting..." << std::endl;
+    cerr << "FAILED: Output file " << output_name << " could not be opened. Quitting..." << endl;
     exit(333);
   }
-  else { cout << "SUCCESS: file " << output_name << " opened!" << std::endl; }
+  else { cout << "SUCCESS: file " << output_name << " opened!" << endl; }
 
   if (undersampling == 0) {
-    std::cout << "WARNING: Undersampling factor is 0, set it to 1" << std::endl;
+    cerr << "WARNING: Undersampling factor is 0, set it to 1" << endl;
+  }
+
+  if (!show_markers && !show_polyline) {
+    show_markers = show_polyline = true;
   }
 
   // Parsing JSON gps database and create a local vector of pointers 
@@ -142,13 +146,13 @@ int main(int argc, char** argv) {
   try {
     gps_records = jsoncons::json::parse_file(input_name);
   }
-  catch (std::exception &e) {
-    std::cout << "Invalid JSON input : " << input_name << std::endl;
-    std::cout << "EXCEPTION: " << e.what() << std::endl;
+  catch (exception &e) {
+    cerr << "Invalid JSON input : " << input_name << endl;
+    cerr << "EXCEPTION: " << e.what() << endl;
     exit(-1);
   }
-  std::vector<jsoncons::json *> gps_records_copy;
-  std::vector<std::vector<jsoncons::json *>> trips;
+  vector<jsoncons::json *> gps_records_copy;
+  vector<vector<jsoncons::json *>> trips;
   int old_counter = 0;
   try {
     if (gps_records.is_array()) {
@@ -188,12 +192,12 @@ int main(int argc, char** argv) {
       trips.push_back(gps_records_copy);
     }
     else {
-      std::cout << "BAD json, not array nor object...?!?!" << std::endl;
+      cerr << "BAD json, not array nor object...?!?!" << endl;
       exit(321);
     }
   }
-  catch (std::exception &e) {
-    std::cout << e.what() << std::endl;
+  catch (exception &e) {
+    cerr << e.what() << endl;
     exit(5);
   }
 
@@ -220,25 +224,25 @@ int main(int argc, char** argv) {
     unsigned int last_timestamp = 0;
     for (size_t j = 0; j < trips[i].size(); j++) {
       if (j % undersampling) continue;
-      std::string tooltip("#" + to_string(j));
+      string tooltip("#" + to_string(j));
       if (verbose) {
         if (trips[i][j]->has_member("date"))
-          tooltip = "date: " + trips[i][j]->at("date").as<std::string>();
+          tooltip = "date: " + trips[i][j]->at("date").as<string>();
         if (trips[i][j]->has_member("alt"))
-          tooltip += "<br />altitude: " + trips[i][j]->at("alt").as<std::string>();
+          tooltip += "<br />altitude: " + trips[i][j]->at("alt").as<string>();
         if (trips[i][j]->has_member("delta_dist"))
-          tooltip += "<br />ds (m): " + trips[i][j]->at("delta_dist").as<std::string>();
+          tooltip += "<br />ds (m): " + trips[i][j]->at("delta_dist").as<string>();
         if (trips[i][j]->has_member("timestamp")) {
           try {
             if (j != 0) last_timestamp = trips[i][j - 1]->at("timestamp").as<unsigned int>();
             else last_timestamp = 0;
-            tooltip += "<br />timestamp: " + std::to_string(trips[i][j]->at("timestamp").as<unsigned int>());
-            tooltip += "<br />dt (s): " + std::to_string(trips[i][j]->at("timestamp").as<unsigned int>() - last_timestamp);
+            tooltip += "<br />timestamp: " + to_string(trips[i][j]->at("timestamp").as<unsigned int>());
+            tooltip += "<br />dt (s): " + to_string(trips[i][j]->at("timestamp").as<unsigned int>() - last_timestamp);
           }
           catch (...) {
             // old format compatibility (crash avoiding)
             try {
-              tooltip += "<br />date:" + trips[i][j]->at("timestamp").as<std::string>();
+              tooltip += "<br />date:" + trips[i][j]->at("timestamp").as<string>();
             }
             catch (...) {
               tooltip += "<br />timestamp: NA";
@@ -246,24 +250,24 @@ int main(int argc, char** argv) {
           }
         }
         if (trips[i][j]->has_member("heading"))
-          tooltip += "<br />heading: " + trips[i][j]->at("heading").as<std::string>();
+          tooltip += "<br />heading: " + trips[i][j]->at("heading").as<string>();
         if (trips[i][j]->has_member("speed"))
-          tooltip += "<br />speed: " + trips[i][j]->at("speed").as<std::string>();
+          tooltip += "<br />speed: " + trips[i][j]->at("speed").as<string>();
         if (trips[i][j]->has_member("enabling"))
-          tooltip += "<br />cause: " + trips[i][j]->at("enabling").as<std::string>();
+          tooltip += "<br />cause: " + trips[i][j]->at("enabling").as<string>();
         if (trips[i][j]->has_member("tracking_glonass")) {
-          tooltip += "<br />glonass sats (used/seen): " + trips[i][j]->at("using_glonass").as<std::string>() + " / " + trips[i][j]->at("tracking_glonass").as<std::string>();
-          tooltip += "<br />gps sats (used/seen): " + trips[i][j]->at("using_gps").as<std::string>() + " / " + trips[i][j]->at("tracking_gps").as<std::string>();
-          tooltip += "<br />total sats (used/seen): " + trips[i][j]->at("using_total").as<std::string>() + " / " + trips[i][j]->at("tracking_total").as<std::string>();
+          tooltip += "<br />glonass sats (used/seen): " + trips[i][j]->at("using_glonass").as<string>() + " / " + trips[i][j]->at("tracking_glonass").as<string>();
+          tooltip += "<br />gps sats (used/seen): " + trips[i][j]->at("using_gps").as<string>() + " / " + trips[i][j]->at("tracking_gps").as<string>();
+          tooltip += "<br />total sats (used/seen): " + trips[i][j]->at("using_total").as<string>() + " / " + trips[i][j]->at("tracking_total").as<string>();
         }
         if (trips[i][j]->has_member("fix"))
-          tooltip += "<br />fix: " + trips[i][j]->at("fix").as<std::string>();
+          tooltip += "<br />fix: " + trips[i][j]->at("fix").as<string>();
         if (trips[i][j]->has_member("global_index"))
-          tooltip += "<br />global index: " + trips[i][j]->at("global_index").as<std::string>();
+          tooltip += "<br />global index: " + trips[i][j]->at("global_index").as<string>();
       }
       output_file
         << "["
-        << std::fixed << std::setprecision(6)
+        << fixed << setprecision(6)
         << (trips[i][j]->has_member("lat") ? trips[i][j]->at("lat").as<double>() : 90.0)
         << ","
         << (trips[i][j]->has_member("lon") ? trips[i][j]->at("lon").as<double>() : 0.0)
@@ -278,17 +282,20 @@ int main(int argc, char** argv) {
   }
   output_file << R"(
         map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 17,
-          center : new google.maps.LatLng(Locations_trip_0[1][0], Locations_trip_0[1][1]),
           mapTypeId : google.maps.MapTypeId.ROADMAP
         });
         
         var infowindow = new google.maps.InfoWindow();
-
         var Marker, i;
+        var bounds = new google.maps.LatLngBounds();
 )";
   for (size_t i = 0; i < trips.size(); i++) {
-    output_file << "/////// TRIP " << i << std::endl;
+    output_file << "/////// TRIP " << i << endl;
+    output_file << R"(
+        for (i = 0; i<Locations_trip_)" << i << R"(.length; i++) {
+          bounds.extend(new google.maps.LatLng(Locations_trip_)" << i << R"([i][0], Locations_trip_)" << i << R"([i][1]));
+        }
+)";
     if (show_markers) {
       output_file << R"(
         for(i = 0; i<Locations_trip_)" << i << R"(.length; i++) {
@@ -340,13 +347,14 @@ int main(int argc, char** argv) {
 )";
     }
   }
-  output_file << endl << "\t\t\t\truler_map = new RulerMap(map)" << endl;
+  output_file << endl << "\t\t\t\tmap.fitBounds(bounds);" << endl;
+  output_file << "\t\t\t\truler_map = new RulerMap(map)" << endl;
   output_file << "\t\t}" << endl;
   for (size_t i = 0; i < trips.size(); i++) {
     output_file << R"(
-    function toggle_trip_)" << i << "(){" << std::endl;
+    function toggle_trip_)" << i << "(){" << endl;
     if (show_polyline) output_file << R"(
-        Trajectory_trip_)" << i << ".setMap(Trajectory_trip_" << i << ".getMap() ? null : map);" << std::endl;
+        Trajectory_trip_)" << i << ".setMap(Trajectory_trip_" << i << ".getMap() ? null : map);" << endl;
     if (show_markers) output_file << R"(
         for (i = 0; i < Markers_trip_)" << i << R"(.length; i++) {
             var mark = Markers_trip_)" << i << R"([i];
