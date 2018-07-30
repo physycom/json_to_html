ee6b6de3aa9287847c579f34b39ff5f8a5812e09
MrRomanMaccaruni <rommacc@yahoo.it>
2018-07-30 19:10:26 +0200

    add build.sh, update submodule

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* && "$1" == "gcc" ]]; then
+  export CC="/usr/local/bin/gcc-8"
+  export CXX="/usr/local/bin/g++-8"
+fi
+
+mkdir -p build
+cd build
+cmake ..
+cmake --build . --target install
+cd ..
