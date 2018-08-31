2a36b98ab225b7460685fac355167a43daa7257c
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-08-31 15:04:00 +0200

    update ci coverage

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "darwin"* && "$1" == "gcc" ]]; then
-  export CC="/usr/local/bin/gcc-8"
-  export CXX="/usr/local/bin/g++-8"
-fi
-
 mkdir -p build
 cd build
 cmake ..
-cmake --build . --target install
+cmake --build .
 cd ..
